b5a6488943a5dcae05f8f81ca5af21772a1be94e
ChenZeqiang <15330223674@163.com>
2019-01-18 11:51:15 +0800

    添加使用旋钮文件

---
+++ SensorFile/RotationSensor_ControllerLED/RotationSensor_ControllerLED.ino
@@ -0,0 +1,14 @@
+int potPin = 0;
+int ledPin = 10;
+
+void setup() {
+  pinMode(ledPin,OUTPUT);
+
+}
+
+void loop() {
+  int sensorValue = analogRead(potPin);
+  int outputValue = map(sensorValue,0,1023,0,255);
+  analogWrite(ledPin,outputValue);
+  delay(100);
+}
